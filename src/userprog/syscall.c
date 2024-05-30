01d50f44f650d32d6cdb0c9f15b74e30feeb97b6
ekbaek <97011645+ekbaek@users.noreply.github.com>
2024-05-30 14:48:13 +0000

    syscall 함수 구현 중, syscall handler 수정함

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,37 +18,68 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   // printf ("system call!\n");
   // thread_exit ();
-  // check validation of the pointers in parameter list
-  if (f->)
 
-  int syscall_number = 0;
-  switch (syscall_number)
-  {
-  case SYS_HALT:
-  halt();
-  case SYS_EXIT:
-  exit();
-  case SYS_EXEC:
-  exec();
-  case SYS_WAIT:
-  wait();
-  case SYS_CREATE:
-  create();
-  case SYS_REMOVE:
-  remove(f);
-  case SYS_OPEN:
-  open();
-  case SYS_FILESIZE:
-  filesize();
-  case SYS_READ:
-  read();
-  case SYS_WRITE:
-  write();
-  case SYS_SEEK:
-  seek();
-  case SYS_TELL:
-  tell();
-  case SYS_CLOSE:
-  close();
-  }
+  // check validation of the pointers in parameter list
+  // copy arguments on the user stack to the kernel
+  // save return value of system call at eax register
+	switch (f->R.rax)
+	{
+	case SYS_HALT:
+		halt();
+		break;
+	case SYS_EXIT:
+		exit(f->R.rdi);
+		break;
+	case SYS_EXEC:
+    f->R.rax = fork(f->R.rdi, f);
+		if (exec(f->R.rdi) == -1)
+      exit(-1);
+		break;
+	case SYS_WAIT:
+		f->R.rax = process_wait(f->R.rdi);
+		break;
+	case SYS_CREATE:
+		f->R.rax = create(f->R.rdi, f->R.rsi);
+		break;
+	case SYS_REMOVE:
+		f->R.rax = remove(f->R.rdi);
+		break;
+	case SYS_OPEN:
+		f->R.rax = open(f->R.rdi);
+		break;
+	case SYS_FILESIZE:
+		f->R.rax = filesize(f->R.rdi);
+		break;
+	case SYS_READ:
+		f->R.rax = read(f->R.rdi, f->R.rsi, f->R.rdx);
+		break;
+	case SYS_WRITE:
+		f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
+		break;
+	case SYS_SEEK:
+		seek(f->R.rdi, f->R.rsi);
+		break;
+	case SYS_TELL:
+		f->R.rax = tell(f->R.rdi);
+		break;
+	case SYS_CLOSE:
+		close(f->R.rdi);
+		break;
+	default:
+		exit(-1);
+		break;
+	}  
+}
+
+// 추가 구현 syscall 함수
+void
+halt (void) 
+{
+  shutdown_power_off();
+}
+
+void 
+exit (int status) 
+{
+  p
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,4 +3,10 @@
 
 void syscall_init (void);
 
+// syscall func
+void halt (void);
+void exit (int status);
+pid_t exec (const char *cmd_line);
+int wait (pid_t pid);
+
 #endif /* userprog/syscall.h */
