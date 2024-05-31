3f7ba7a6b5a565a66551c6c26ba5e66a17b0c786
ekbaek <97011645+ekbaek@users.noreply.github.com>
2024-05-31 03:51:55 +0000

    syscall함수 완성, fork만 코드 구현 해야함

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,77 +5,87 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+static void check_verify (void *address);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init(&filesys_lock);
 }
 
-// implement
+void
+check_verify (void * address)
+{
+  if (!is_user_vaddr(address) || address == NULL)
+    exit (-1);
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  // printf ("system call!\n");
-  // thread_exit ();
-
-  // copy arguments on the user stack to the kernel
-  // save return value of system call at eax register
-	switch (f->R.rax)
-	{
-	case SYS_HALT:
-		halt();
-		break;
-	case SYS_EXIT:
-		exit(f->R.rdi);
-		break;
-	case SYS_EXEC:
-    f->R.rax = fork(f->R.rdi, f);
-		if (exec(f->R.rdi) == -1)
-      exit(-1);
-		break;
-	case SYS_WAIT:
-		f->R.rax = process_wait(f->R.rdi);
-		break;
-	case SYS_CREATE:
-		f->R.rax = create(f->R.rdi, f->R.rsi);
-		break;
-	case SYS_REMOVE:
-		f->R.rax = remove(f->R.rdi);
-		break;
-	case SYS_OPEN:
-		f->R.rax = open(f->R.rdi);
-		break;
-	case SYS_FILESIZE:
-		f->R.rax = filesize(f->R.rdi);
-		break;
-	case SYS_READ:
-		f->R.rax = read(f->R.rdi, f->R.rsi, f->R.rdx);
-		break;
-	case SYS_WRITE:
-		f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
-		break;
-	case SYS_SEEK:
-		seek(f->R.rdi, f->R.rsi);
-		break;
-	case SYS_TELL:
-		f->R.rax = tell(f->R.rdi);
-		break;
-	case SYS_CLOSE:
-		close(f->R.rdi);
-		break;
+  switch (f->eax)
+  {
+		case SYS_HALT:
+			halt();
+			break;
+		case SYS_EXIT:
+			exit(f->edi);
+			break;
+		case SYS_EXEC:
+			f->eax = fork(f->edi, f);
+			if (exec(f->edi) == -1)
+				exit(-1);
+			break;
+		case SYS_WAIT:
+			f->R.rax = process_wait(f->edi); //process wait 수정하러 가야함
+			break;			
+    case SYS_CREATE:
+      f->eax = create (f->edi, f->esi);
+      break;
+    case SYS_REMOVE:
+      f->eax = remove (f->edi);
+      break;
+    case SYS_OPEN:
+      f->eax = open (f->edi);
+      break;
+    case SYS_FILESIZE:
+      f->eax = filesize (f->edi);
+      break;
+    case SYS_READ:
+      f->eax = read (f->edi, f->esi, f->edx);
+      break;
+    case SYS_WRITE:
+      f->eax = write (f->edi, f->esi, f->edx);
+      break;
+    case SYS_SEEK:
+      seek (f->edi, f->esi);
+      break;
+    case SYS_TELL:
+      f->eax = tell (f->edi);
+      break;
+    case SYS_CLOSE:
+      close (f->edi);
+      break;
 	default:
 		exit(-1);
 		break;
 	}  
+  }
+  printf ("system call!\n");
+  thread_exit ();
 }
 
-// check validation of the pointers in parameter list
-void
-check_validation (struct intr_frame *f)
-{
-
-}
 
 // 추가 구현 syscall 함수
 void
@@ -93,6 +103,9 @@ exit (int status)
   thread_exit();
 }
 
+fork
+
+
 pid_t
 exec (const char *cmd_line) 
 {
@@ -103,45 +116,6 @@ exec (const char *cmd_line)
   process_execute(cmd_line);
 }
 
-int
-wait (pid_t pid)
-{
-  process_wait(pid);
-}
-
-// create file
-bool
-create (const char *file_name, off_t initial_size)
-{
-  check_validation(file_name);
-  return filesys_create(file_name, initial_size);
-}
-
-fork
-
-// delete a fiel
-bool
-remove (const char *file_name)
-{
-  check_validation(file_name);
-  return filesys_remove(file_name);
-}
-
-//open file
-
-open
-
-// return file size
-filesize 
 
 
-read 
-
-write 
-
-seek 
-
-tell 
-
-close 
 
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,12 +1,14 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+
+struct lock filesys_lock;
 void syscall_init (void);
 
 // syscall func
 void halt (void);
 void exit (int status);
 pid_t exec (const char *cmd_line);
-int wait (pid_t pid);
+
 
 #endif /* userprog/syscall.h */
